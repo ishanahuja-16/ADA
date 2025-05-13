acf55c3dbae234a5abca399b6349b44e8180e6a3
ishanahuja-16 <ishanahuja.cs23@bmsce.ac.in>
2025-05-13 09:47:42 +0530

    Add files via upload

---
+++ ish4.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include<time.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    clock_t start, end;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    start = clock();
+    quickSort(arr, 0, n - 1);
+    end = clock();
+
+    double time_taken = (double)(end-start)/(CLK_TCK);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    printf("Time Complexity:%d", time_taken);
+
+    return 0;
+}
